47f30f772ea0924fbe8d25c3c6a9fcd2ae4a97d8
Davide Clemente <davide11clemente@gmail.com>
2024-04-19 22:52:23 +0100

    Choose currencies in dropdowns

--- src/cogs/currency.py
+++ src/cogs/currency.py
@@ -25,16 +25,39 @@ class Currency(commands.Cog):
             currency2 (str): Currency to convert to
             amount (float): Amount to be converted
         """
-        currency1 = currency1.upper()
-        currency2 = currency2.upper()
-        if currency1 not in self.currencies:
-            await itr.response.send_message(f'{currency1} is not supported!', ephemeral=True)
 
-        if currency2 not in self.currencies:
-            await itr.response.send_message(f'{currency2} is not supported!', ephemeral=True)
+        drp_from = discord.ui.Select(
+            placeholder="Select currency to convert from",
+            options=[discord.SelectOption(label=currency)
+                     for currency in self.currencies]
+        )
+
+        drp_to = discord.ui.Select(
+            placeholder="Select currency to convert to",
+            options=[discord.SelectOption(label=currency)
+                     for currency in self.currencies]
+        )
+
+        btn = discord.ui.Button(
+            label="Convert", style=discord.ButtonStyle.primary)
+
+        view = discord.ui.View().add_item(drp_from).add_item(drp_to).add_item(btn)
+
+        await itr.response.send_message("Select currencies to convert", view=view)
+
+        from_resp = await self.client.wait_for('select_option', check=lambda m: m.user.id == itr.user.id)
+        to_resp = await self.client.wait_for('select_option', check=lambda m: m.user.id == itr.user.id)
+
+        # currency1 = currency1.upper()
+        # currency2 = currency2.upper()
+        # if currency1 not in self.currencies:
+        #     await itr.response.send_message(f'{currency1} is not supported!', ephemeral=True)
+
+        # if currency2 not in self.currencies:
+        #     await itr.response.send_message(f'{currency2} is not supported!', ephemeral=True)
 
         api_response_json = requests.get(
-            f'{settings.CURRENCY_API}&currencies={currency2}&base_currency={currency1}').json()
+            f'{settings.CURRENCY_API}&currencies={to_resp.values[0]}&base_currency={from_resp.values[0]}').json()
 
         if (api_response_json['data'] is not None):
             data = api_response_json['data']
