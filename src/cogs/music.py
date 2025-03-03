e09c7903d429e224954e3726178034ebbfb65653
Davide Clemente <1190497@isep.ipp.pt>
2025-03-03 16:34:39 +0000

    fix: Queue cleanup

--- src/cogs/music.py
+++ src/cogs/music.py
@@ -112,6 +112,22 @@ class Music(commands.Cog):
             self.queue.pop(self.queue_index - 3)
             self.queue_index -= 1  # Adjust index to keep it in sync
 
+    def get_current_song_from_queue(self):
+        return self.queue[self.queue_index]['song']
+
+    def get_previous_song(self):
+        return self.queue[
+            self.queue_index - 1]['song'] if len(self.queue) > 1 and self.queue_index >= 1 else None
+
+    def is_queue_empty(self):
+        return len(self.queue) == 0
+
+    def clear_queue_impl(self):
+        current_music = self.get_current_song_from_queue()
+        if self.queue_index > 0:
+            self.queue = [current_music]
+            self.queue_index = 0
+
     @commands.Cog.listener()
     async def on_voice_state_update(self, member: discord.Member, before: discord.VoiceState,
                                     after: discord.VoiceState):
@@ -140,9 +156,9 @@ class Music(commands.Cog):
     async def reaction_next(self, channel: discord.TextChannel, reaction: discord.Reaction, user: discord.User):
         msg = None
         if not self.in_voice_channel():
-            msg = await channel.send("You need to be in a voice channel to you this command")
+            msg = await channel.send("❌ You need to be in a voice channel to you this command ❌")
         elif not self.exists_next_song_in_queue():
-            msg = await channel.send("There is no next song in the queue. Replaying current song")
+            msg = await channel.send("ℹ️ There is no next song in the queue. Replaying current song ℹ️")
             self.voice_client.pause()
             await self.play_music(reaction.message.channel.id, user)
             self.reset_inactivity_timer()
@@ -151,7 +167,7 @@ class Music(commands.Cog):
             self.voice_client.pause()
             played = await self.play_music(reaction.message.channel.id, user)
             if not played:
-                msg = await channel.send('There are no songs to be played in the queue')
+                msg = await channel.send('⚠️ There are no songs to be played in the queue ⚠️')
             else:
                 self.clean_queue()
                 self.reset_inactivity_timer()
@@ -161,9 +177,9 @@ class Music(commands.Cog):
     async def reaction_previous(self, channel: discord.TextChannel, reaction: discord.Reaction, user: discord.User):
         msg = None
         if not self.voice_client:
-            msg = await channel.send("You need to be in a voice channel to use this command")
+            msg = await channel.send("❌ You need to be in a voice channel to use this command ❌")
         elif self.queue_index <= 0:
-            msg = await channel.send("There is no previous song in the queue. Replaying current song")
+            msg = await channel.send("ℹ️ There is no previous song in the queue. Replaying current song ℹ️")
             self.voice_client.pause()
             await self.play_music(reaction.message.channel.id, user)
             self.reset_inactivity_timer()
@@ -172,21 +188,21 @@ class Music(commands.Cog):
             self.voice_client.pause()
             played = await self.play_music(reaction.message.channel.id, user)
             if not played:
-                msg = await channel.send('There are no songs to be played in the queue')
+                msg = await channel.send('⚠️ There are no songs to be played in the queue ⚠️')
         if msg:
             await delete_message(msg)
 
     async def reaction_play_pause(self, channel: discord.TextChannel, reaction: discord.Reaction, user: discord.User):
         msg = None
         if not self.voice_client:
-            msg = await channel.send("There is no audio to be paused at the moment")
+            msg = await channel.send("⚠️ There is no audio to be paused at the moment ⚠️")
         elif self.is_playing:
-            msg = await channel.send(f"Audio Paused by {user.display_name}")
+            msg = await channel.send(f"⏯️ Audio Paused by {user.display_name}")
             self.is_playing = False
             self.voice_client.pause()
             self.start_inactivity_timer(5)
         else:
-            msg = await channel.send(f"Audio Resumed by {user.display_name}")
+            msg = await channel.send(f"⏯️ Audio Resumed by {user.display_name}")
             self.is_playing = True
             self.voice_client.resume()
             self.reset_inactivity_timer()
@@ -208,14 +224,14 @@ class Music(commands.Cog):
             self.voice_client = await voice_channel.connect()
             if self.voice_client is None:
                 channel = self.client.get_channel(text_channel)
-                await channel.send('Could not connect to the channel')
+                await channel.send('⚠️ Could not connect to the channel ⚠️')
         else:
             # Move to channel
             await self.voice_client.move_to(voice_channel)
 
     async def pause(self, itr: discord.Interaction):
         if not self.voice_client:
-            await itr.response.send_message('There is no audio playing at the moment')
+            await itr.response.send_message('❌ There is no audio playing at the moment ❌')
         elif self.is_playing:
             self.is_playing = False
             self.voice_client.pause()
@@ -231,7 +247,7 @@ class Music(commands.Cog):
             self.queue_index += 1
 
             song = self.get_current_song_from_queue()
-            message = create_playing_embed("Now playing", user, song)
+            message = create_playing_embed("▶️ Now playing", user, song)
 
             channel = self.client.get_channel(self.last_played_msg.channel.id)
 
@@ -276,16 +292,6 @@ class Music(commands.Cog):
 
         self.client.loop.create_task(play_audio_thread())
 
-    def get_current_song_from_queue(self):
-        return self.queue[self.queue_index]['song']
-
-    def get_previous_song(self):
-        return self.queue[
-            self.queue_index - 1]['song'] if len(self.queue) > 1 and self.queue_index >= 1 else None
-
-    def is_queue_empty(self):
-        return len(self.queue) == 0
-
     async def play_music(self, channel_id, user):
         """Plays a song
 
@@ -305,7 +311,7 @@ class Music(commands.Cog):
             if self.last_played_msg:
                 await self.last_played_msg.delete()
 
-            embed = create_playing_embed("Now playing", user, song)
+            embed = create_playing_embed("▶️ Now playing", user, song)
             msg = await channel.send(embed=embed, silent=True)
             self.last_played_msg = msg
             await add_reactions(msg)
@@ -327,9 +333,9 @@ class Music(commands.Cog):
         if itr.user.voice:
             user_channel = itr.user.voice.channel
             await self.join_voice_channel(itr, user_channel)
-            await itr.response.send_message(f'DouraBot has joined {user_channel}')
+            await itr.response.send_message(f'🎉 DouraBot has joined {user_channel} 🎉')
         else:
-            await itr.response.send_message('You need to be connected to a voice channel')
+            await itr.response.send_message('⚠️ You need to be connected to a voice channel ⚠️')
 
     @app_commands.command(name='leave', description="leave voice channel")
     async def leave(self, itr: discord.Interaction):
@@ -341,9 +347,9 @@ class Music(commands.Cog):
         if self.in_voice_channel():
             await self.disconnect()
             self.voice_client = None
-            await itr.response.send_message('DouraBot has left the voice channel')
+            await itr.response.send_message('ℹ️ **DouraBot** has left the voice channel ℹ️')
         else:
-            await itr.response.send_message('DouraBot is not in a voice channel')
+            await itr.response.send_message('ℹ️ **DouraBot** is not in a voice channel ℹ️')
 
     async def search_youtube(self, search: str):
         """Searches YouTube for results based on url or search params
@@ -427,17 +433,17 @@ class Music(commands.Cog):
         channel = itr.channel
         self.logger.info(f'User {itr.user.display_name} called play/{song}')
         if not itr.user.voice:
-            await itr.followup.send('You need to be connected to a voice channel')
+            await itr.followup.send('⚠️ You need to be connected to a voice channel ⚠️')
 
         # Play the musics in the queue
         if song is None:
             if self.is_queue_empty():
-                await itr.followup.send('There are no songs to be played in the queue')
+                await itr.followup.send('❌ There are no songs to be played in the queue ❌')
                 return
             if not self.is_playing:
                 played = await self.play_music(itr.channel.id, itr.user)
                 if not played:
-                    await itr.followup.send('There are no songs to be played in the queue')
+                    await itr.followup.send('❌ There are no songs to be played in the queue ❌')
             else:
                 self.is_playing = True
                 self.voice_client.resume()
@@ -447,39 +453,39 @@ class Music(commands.Cog):
             # Check if music found
             search_results = await self.search_youtube(song)
             if not search_results:
-                msg = await itr.followup.send('Could not find the song')
+                msg = await itr.followup.send('❌ Could not find the song ❌')
                 await delete_message(msg)
                 return
 
             song_info = await self.extract_youtube(search_results[0])
             if not song_info:
-                await itr.followup.send('Could not play the song.')
+                await itr.followup.send('❌ Could not play the song ❌')
                 return
 
             user_channel = itr.user.voice.channel
             # Add music to queue
             for song in song_info:
                 self.queue.append({'song': song, 'channel': user_channel})
-            await channel.send(f'Added {len(song_info)} songs to the queue')
+            await channel.send(f'📜 Added {len(song_info)} songs to the queue 📜')
 
             # If not active playing, play the music
             if not self.is_playing:
                 # If paused, just add to queue
                 if self.voice_client and self.voice_client.is_paused():
-                    embed = create_playing_embed("Added to queue", itr.user, song_info)
+                    embed = create_playing_embed("📜 Added to queue 📜", itr.user, song_info)
 
                     msg2 = await itr.followup.send(embed=embed, silent=True)
                     await delete_message(msg2)
                 else:
-                    tmp_msg = await itr.followup.send("Connected and playing...")
+                    tmp_msg = await itr.followup.send("▶️ Connected and playing...")
                     await delete_message(tmp_msg)
                     played = await self.play_music(itr.channel.id, itr.user)
                     if not played:
-                        await itr.followup.send('There are no songs to be played in the queue')
+                        await itr.followup.send('❌ There are no songs to be played in the queue ❌')
             # Otherwise, just add it to the queue
             else:
                 embed = create_playing_embed(
-                    "Added to queue", itr.user, song_info)
+                    "📜 Added to queue 📜", itr.user, song_info)
                 msg2 = await itr.followup.send(embed=embed, silent=True)
                 await delete_message(msg2)
 
@@ -510,17 +516,29 @@ class Music(commands.Cog):
         try:
             next_songs = self.queue[self.queue_index + 1:self.queue_index + 10]
             song_list = [
-                f'[{song["song"]["title"]}]({song["song"]["link"]})'
-                for song in next_songs
+                f'{i+1}. [{song["song"]["title"]}]({song["song"]["link"]})'
+                for i, song in enumerate(next_songs)
             ]
             songs_text = "\n".join(song_list)
             if len(songs_text) > 1024:
                 songs_text = songs_text[:1021] + "..."  # Ensure it doesn't exceed 1024
 
             embed.add_field(name="PLAYING NEXT", value=songs_text, inline=False)
-        except Exception:
+        except Exception as e:
             pass
         await itr.followup.send(embed=embed, silent=True)
+
+    @app_commands.command(name='clear_queue', description="clear the current queue")
+    async def clear_queue(self, itr: discord.Interaction):
+        """Clear the current queue
+
+        Args:
+            itr (discord.Interaction): Discord interaction
+        """
+        await itr.response.defer()
+        self.clear_queue_impl()
+        msg = await itr.followup.send('🗑️ Queue cleared! 🗑️', silent=True)
+        await delete_message(msg)
 
 
 async def setup(client: commands.Bot) -> None:
