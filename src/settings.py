f1c91ebc51c9070ca166de734b6839aa59ecc05f
Davide Clemente <49814793+DavideClemente@users.noreply.github.com>
2025-04-12 17:03:49 +0100

    Spotify Playlists (#34)

--- src/settings.py
+++ src/settings.py
@@ -26,6 +26,8 @@ DISCORD_TOKEN = os.getenv(
     "TOKEN_DEV") if IS_DEV else os.getenv("TOKEN_PROD")
 DISCORD_ID = int(os.getenv("DOURADINHOS"))
 DISCORD_GUILD = discord.Object(id=int(os.getenv("DOURADINHOS")))
+SPOTIFY_CLIENT_ID = os.getenv("SPOTIFY_CLIENT_ID")
+SPOTIFY_CLIENT_SECRET = os.getenv("SPOTIFY_CLIENT_SECRET")
 
 DOURADINHOS_COLOR = '0x#f28e0e'
 DOURADINHOS_IMAGE = 'https://www.nit.pt/wp-content/uploads/2016/10/ed3647fa-e8e1-47da-984f-4f166d66fa1c-754x394.jpg'
