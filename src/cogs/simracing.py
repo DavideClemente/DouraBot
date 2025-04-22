f92819d22c412b5fc5a754689a354e97b8bedeaf
Davide Clemente <davide11clemente@gmail.com>
2025-04-22 18:08:17 +0100

    Deal with exceptions

--- src/cogs/simracing.py
+++ src/cogs/simracing.py
@@ -21,18 +21,24 @@ class SimRacing(commands.Cog):
         :param fuel_per_lap: Fuel consumption per lap in liters
         :return: Recommended fuel in liters
         """
+        try:
+            minutes, seconds = map(int, lap_time.split(':'))
+            lap_time_seconds = minutes * 60 + seconds
 
-        minutes, seconds = map(int, lap_time.split(':'))
-        lap_time_seconds = minutes * 60 + seconds
+            total_time_seconds = total_time * 60
+            laps = total_time_seconds / lap_time_seconds
 
-        total_time_seconds = total_time * 60
-        laps = total_time_seconds / lap_time_seconds
+            total_fuel = math.ceil(laps * fuel_per_lap)
+            # Calculate recommended fuel by adding a 5% safety margin
+            recommended_fuel = math.ceil(total_fuel * 1.05)
 
-        total_fuel = math.ceil(laps * fuel_per_lap)
-        # Calculate recommended fuel by adding a 5% safety margin
-        recommended_fuel = math.ceil(total_fuel * 1.05)
-
-        await itr.response.send_message(f"Minimum fuel needed for the race: {total_fuel:.2f} liters ⛽.\nRecommended fuel: {recommended_fuel} liters ⚠️")
+            await itr.response.send_message(f"⛽ Minimum fuel needed for the race: {total_fuel:.2f} liters ⛽.\n⚠️ Recommended fuel: {recommended_fuel} liters ⚠️", ephemeral=True)
+        except ValueError:
+            await itr.response.send_message("Invalid lap time format. Please use mm:ss.", ephemeral=True)
+        except ZeroDivisionError:
+            await itr.response.send_message("Lap time cannot be zero.", ephemeral=True)
+        except Exception as e:
+            await itr.response.send_message(f"An error occurred: {str(e)}", ephemeral=True)
 
     @app_commands.command(name='calculate_fuel_laps',
                           description="Calculate the fuel needed for a race based on the total laps")
@@ -45,13 +51,17 @@ class SimRacing(commands.Cog):
         :return: Recommended fuel in liters
         """
 
-        total_fuel = math.ceil(laps * fuel_per_lap)
-        # Calculate recommended fuel by adding a 5% safety margin
-        recommended_fuel = math.ceil(total_fuel * 1.05)
+        try:
+            total_fuel = math.ceil(laps * fuel_per_lap)
+            # Calculate recommended fuel by adding a 5% safety margin
+            recommended_fuel = math.ceil(total_fuel * 1.05)
 
-        await itr.response.send_message(
-            f"Minimum fuel needed for the race: {total_fuel:.2f} liters ⛽.\nRecommended fuel: {recommended_fuel} liters ⚠️")
-
+            await itr.response.send_message(
+                f"⛽ Minimum fuel needed for the race: {total_fuel:.2f} liters ⛽.\n⚠️ Recommended fuel: {recommended_fuel} liters ⚠️", ephemeral=True)
+        except ValueError | TypeError:
+            await itr.response.send_message("Invalid values. Please use valid values.", ephemeral=True)
+        except Exception as e:
+            await itr.response.send_message(f"An error occurred: {str(e)}", ephemeral=True)
 
 async def setup(client: commands.Bot) -> None:
     await client.add_cog(SimRacing(client))
